db2b90a1b5027a644b48fe4c0f8988755adab292
ediix164-a11y <ediix.164@gmail.com>
2026-04-13 00:22:27 +0900

    Update 4app.py

--- 4app.py
+++ 4app.py
@@ -213,8 +213,7 @@ if st.session_state.scan_results is not None:
 
     st.link_button(
         f"📊 {code} {row['銘柄名']} チャート",
-        f"https://jp.tradingview.com/symbols/TSE-{code}/"
-    )
+        f"https://jp.tradingview.com/symbols/TSE-{code}/" )
 
 # =========================
 # 自動更新
